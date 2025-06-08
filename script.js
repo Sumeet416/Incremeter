62d81c4c81831f9550781f002a1ec3f97fda7cea
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-06-08 23:30:25 +0530

    increment and decrement buttons added

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const countbtn = document.querySelector("#count");
+const increment = document.querySelector("#incre");
+const decrement = document.querySelector("#decre")
 const target = document.querySelector("#target");
 const resetbtn = document.querySelector("#reset");
 let counter = 0
-countbtn.addEventListener("click", function(){
+increment.addEventListener("click", function(){
     counter++
+    target.innerHTML = `${counter}`
+
+})
+
+decrement.addEventListener("click", function(){
+    counter--
+    if(counter < 0){
+        counter = 0
+    }
     target.innerHTML = `${counter}`
 
 })
